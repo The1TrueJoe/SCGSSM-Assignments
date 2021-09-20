a9575c2a7775872089564757fd13b241e1e011c0
The1TrueJoe <jttelaak@hotmail.com>
2021-09-20 09:54:40 -0400

    Add ops

--- CSC111/SML/CPU.cpp
+++ CSC111/SML/CPU.cpp
@@ -4,6 +4,87 @@
 
 using namespace std;
 
-int main() {
-    cout << CPU::LOAD << endl;
+void CPU::Start() {  
+    
 }
+
+// OP
+int CPU::OP(int op_code, int memmory_address) {
+    switch (op_code) {
+        case CPU::READ:         return OP_READ(memmory_address);
+        case CPU::WRITE:        return OP_WRITE(memmory_address);
+        case CPU::LOAD:         return OP_LOAD(memmory_address);
+        case CPU::STORE:        return OP_STORE(memmory_address);
+        case CPU::ADD:          return OP_ADD(memmory_address);
+        case CPU::SUB:          return OP_SUB(memmory_address);
+        case CPU::DIV:          return OP_DIV(memmory_address);
+        case CPU::MULT:         return OP_MULT(memmory_address);
+        case CPU::BRANCH:       return OP_BRANCH(memmory_address);
+        case CPU::BRANCHNEG:    return OP_BRANCHNEG(memmory_address);
+        case CPU::BRANCHZERO:   return OP_READ(memmory_address);
+        case CPU::HALT:         return OP_HALT(memmory_address);
+
+    }
+
+}
+
+// Read a word from the keyboard into the specified memory location
+int CPU::OP_READ(int memmory_address) {
+
+}
+
+// Write a word from a specified memory location onto the screen
+int CPU::OP_WRITE(int memmory_address) {
+    cout << memmory.Load(memmory_address) << endl;
+
+}      
+        
+// Load a word from a specified location in memory into the accumulator 
+int CPU::OP_LOAD(int memmory_address) {
+
+}
+
+// Store a word from the accumulator into a specified location in memory
+int CPU::OP_STORE(int memmory_address) {
+
+}  
+        
+// Add a word from a specified location in memory to the accumulator
+int CPU::OP_ADD(int memmory_address) {
+
+}
+
+// Subtract a word from a location in memory from the accumulator
+int CPU::OP_SUB(int memmory_address) {
+
+}     
+
+// Divide a word from a location into the value in the accumulator
+int CPU::OP_DIV(int memmory_address) {
+
+}
+
+// Multiply a word from a location into the value in the accumulator
+int CPU::OP_MULT(int memmory_address) {
+
+}
+
+// Branch to the specified location in memory
+int CPU::OP_BRANCH(int memmory_address) {
+
+}
+
+// Branch to the specified location if the accumulator is negative
+int CPU::OP_BRANCHNEG(int memmory_address) {
+
+}
+
+// Branch to the specified location if the accumulator is zero 
+int CPU::OP_BRANCHZERO(int memmory_address) {
+
+}
+        
+// Halt
+int CPU::OP_HALT(int memmory_address) {
+
+}     
--- CSC111/SML/CPU.h
+++ CSC111/SML/CPU.h
@@ -4,10 +4,24 @@
  * CPU Based on SML
  */  
 
+#include "RAM.h"
+
 class CPU {
     public:
-        // Accumulator
+        // Accumulator stores a word that cpu is working on
         int Accumulator;
+
+        // Instruction Pointer 
+        int InstructionPointer;
+
+        // Starts
+        void Start();
+
+        // Operate
+        int OP(int op_code, int memmory_adress);
+
+        // Memmory
+        RAM memmory;
 
         // OPCODES
         static const int READ       = 10; // Read a word from the keyboard into the specified memory location
@@ -24,5 +38,19 @@ class CPU {
         static const int HALT       = 43; // Halt
 
     private:
+
+        // Operations
+        int OP_READ(int);       // Read a word from the keyboard into the specified memory location
+        int OP_WRITE(int);      // Write a word from a specified memory location onto the screen
+        int OP_LOAD(int);       // Load a word from a specified location in memory into the accumulator 
+        int OP_STORE(int);      // Store a word from the accumulator into a specified location in memory
+        int OP_ADD(int);        // Add a word from a specified location in memory to the accumulator
+        int OP_SUB(int);        // Subtract a word from a location in memory from the accumulator
+        int OP_DIV(int);        // Divide a word from a location into the value in the accumulator
+        int OP_MULT(int);       // Multiply a word from a location into the value in the accumulator
+        int OP_BRANCH(int);     // Branch to the specified location in memory
+        int OP_BRANCHNEG(int);  // Branch to the specified location if the accumulator is negative
+        int OP_BRANCHZERO(int); // Branch to the specified location if the accumulator is zero 
+        int OP_HALT(int);       // Halt
 
 };
