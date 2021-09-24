f566eadb9d1efabab8e265c0fd69c0aa63dfd947
The1TrueJoe <jttelaak@hotmail.com>
2021-09-24 16:35:14 -0400

    Add file reading

--- CSC111/SML/RAM.cpp
+++ CSC111/SML/RAM.cpp
@@ -3,6 +3,9 @@
 
 #include <array>
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -11,8 +14,47 @@ RAM::RAM() {
 }
 
 void RAM::LoadFile(string file_location) {
-    
+    // Setup vars
+    string line;
+    int memory_byte;
 
+    // Memory array
+    array<int, 100> new_memory;
+
+    // File stream
+    ifstream myfile (file_location);
+
+    // While file is open
+    if (myfile.is_open()) {
+        // Current line
+        int line_count = 0;
+
+        // While a line exists
+        while(!myfile.eof()) {
+            // Clear variables
+            line = "";
+            memory_byte = 0;
+
+            // Read the line
+            getline(myfile, line);
+
+            // Format as int
+            stringstream obj;
+            obj << line;
+            obj >> memory_byte;
+
+            // Assign to memory array
+            new_memory[line_count] = memory_byte;
+
+            // Increment line count
+            line_count += 1;
+
+        }
+
+        // Close
+        myfile.close();
+        
+    }
 }
 
 // Reset the ram with a new array
