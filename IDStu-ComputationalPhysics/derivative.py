74832b7c905d4f4b4e8bf5cafb6b89a371526b38
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2022-02-04 14:15:56 -0500

    Calc assignments

---
+++ IDStu-ComputationalPhysics/derivative.py
@@ -0,0 +1,47 @@
+from numpy import sin, cos, fabs
+
+
+def f(x):
+    return sin(x)
+
+
+"""Please write a program to compute the forward derivative, backward derivative, centered derivative and actual 
+derivative of sin(x) evaluated at x = 0.50 using a small value of h and the errors in the three methods.  Which is 
+the most accurate? """
+
+
+def forward_difference(x, h):
+    return (f(x + h) - f(x)) / h
+
+
+def backward_difference(x, h):
+    return (f(x) - f(x - h)) / h
+
+
+def centered_difference(x, h):
+    return (f(x + h) - f(x - h)) / 2 / h
+
+
+def actual_derivative(x):
+    return cos(x)
+
+
+def actual_second_derivative(x):
+    return -1 * sin(x)
+
+
+def second_derivative(x, h):
+    return (f(x + h) - 2 * f(x) + f(x - h)) / h / h
+
+
+def interpolate(a, b):
+    return ((f(b) - f(a)) / (b - a)) * (x - a) + f(a)
+
+
+x = .5
+h = .1
+abs = actual_derivative(x)
+for func in [forward_difference(x, h), backward_difference(x, h), centered_difference(x, h)]:
+    print(fabs(abs - func))
+print(fabs(actual_second_derivative(x) - second_derivative(x, h)))
+print(fabs(interpolate(x - .05, x + .05) - sin(x)))
---
+++ IDStu-ComputationalPhysics/romberg.py
@@ -0,0 +1,57 @@
+from numpy import arange, sin, sqrt, power, fabs, sum
+
+
+def f(x):
+    return power(sin(sqrt(100 * x)), 2)
+
+
+def trapezoidal_integral(a, b, n):  # from 5.2
+    h = (b - a) / n
+    x = arange(1, n)
+    return h * ((f(a) / 2) + (f(b) / 2) + sum(f(a + x * h)))
+
+
+def adaptive_trapezoidal(a, b, n, desired_error):
+    last = trapezoidal_integral(a, b, n)
+    err = 9e4
+    print(desired_error)
+    while err > desired_error:
+        print(f'slices: {n}')
+        print(f'estimate: {last}')
+        print(f'error: {err}')
+        n *= 2
+        h = (b - a) / n
+        k = arange(1, n - 1, 2)
+        new = (last / 2) + h * sum(f(a + k * h))
+        err = fabs((new - last)) / 3
+        last = new
+    return last
+
+
+def romberg_integration(a, b, desired_error):
+    r = [[trapezoidal_integral(a, b, 1)], [trapezoidal_integral(a, b, 2)]]
+    i = 1
+    j = 0
+    n = 3
+    err = 9e4
+    while err > desired_error:
+        while i > j:
+            r[i].append(r[i][j] + (1 / (power(4, j + 1) - 1) * (r[i][j] - r[i - 1][j])))
+
+            err = (r[i][j] - r[i - 1][j]) / (power(4, j + 1) - 1)
+            if err < desired_error:
+                print(i, j, err)
+                for i2 in range(len(r)):
+                    print([term for term in r[i2]])
+                return r[i][j]
+            j += 1
+
+        r.append([trapezoidal_integral(a, b, n)])
+        i += 1
+        n += 1
+        j = 0
+    return r[i][j]
+
+
+adaptive_trapezoidal(0, 1, 1, 1e-6)
+romberg_integration(0, 1, 1e-6)
